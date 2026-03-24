3928221997a18cee2b613b0c189b7606391f5a54
ilodev <ilodev@users.noreply.github.com>
2026-03-24 12:56:14 +0100

    Update QuickTips.cs
    No need for Visual Scripting package.
    This doesn't need a PR, needs a quick fix.

--- Editor/Installers/QuickTips.cs
+++ Editor/Installers/QuickTips.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using Unity.VisualScripting.YamlDotNet.Core;
 using UnityEditor;
 using UnityEngine;
 
