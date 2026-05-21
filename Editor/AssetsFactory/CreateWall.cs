2af7f6e0ce0505a01844d7187edc92cbb386b2b6
ilodev <ilodev@users.noreply.github.com>
2026-05-22 01:08:00 +0200

    Update CreateWall.cs
    Improved wall creation process

--- Editor/AssetsFactory/CreateWall.cs
+++ Editor/AssetsFactory/CreateWall.cs
@@ -1,4 +1,6 @@
+using Assets.Scripts.Objects;
 using UnityEditor;
+using UnityEngine;
 
 namespace ilodev.stationeersmods.tools.assetsfactory
 {
@@ -15,6 +17,49 @@ namespace ilodev.stationeersmods.tools.assetsfactory
         public static void CreateWallAsset()
         {
             StationeersAssetFactory.CreateAsset(new CreateWall());
+        }
+
+        /// <summary>
+        /// Complete gameobject setup
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="generatedType"></param>
+        public override void ConfigureGameObject(GameObject gameObject, System.Type generatedType)
+        {
+            MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+            BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+            boxCollider.center = new Vector3(0, 0, 0.065f);
+            boxCollider.size = new Vector3(2, 2, 0.13f);
+
+            gameObject.AddComponent(generatedType);
+
+            // Add default build state
+            // TODO: move this to a helper function
+            Structure structure = gameObject.GetComponent<Structure>();
+            // Add default build state
+            BuildState defaultBuildState = new BuildState();
+            defaultBuildState.BlockAir = true;
+            defaultBuildState.BlockLight = true;
+            defaultBuildState.BlockGravity = true;
+            defaultBuildState.Visualizer = meshRenderer;
+            defaultBuildState.RenderMode = BuildStateRenderMode.OnMineAndPreviousStates;
+            structure.BuildStates = new System.Collections.Generic.List<BuildState>();
+            structure.BuildStates.Add(defaultBuildState);
+
+
+            // Add sound blocker
+            // TODO: Move this to a helper function
+            GameObject blockSound = new GameObject("BlockSound");
+            blockSound.transform.parent = gameObject.transform;
+            blockSound.layer = LayerMask.NameToLayer("BlockSound");
+            BoxCollider blockSoundCollider = blockSound.AddComponent<BoxCollider>();
+            blockSoundCollider.center = boxCollider.center;
+            blockSoundCollider.size = boxCollider.size;
+
+
+            EditorUtility.SetDirty(gameObject);
         }
     }
 }
